6ba2568c230bac382890dcadeb7a7ce5ba61ce47
sakshi\sakshigarg <gargs62@gmail.com>
2019-03-07 15:41:26 +0530

    Removed Superimposition

--- removingSuperimposition.py
+++ removingSuperimposition.py
@@ -1,10 +1,11 @@
+source = "C:\\Users\\sakshigarg\\PycharmProjects\\btpMidterm\\Original_Dataset\\"
+destination="C:\\Users\\sakshigarg\\PycharmProjects\\btpMidterm\\Removedsuperimposition_Dataset\\"
 fil1 = "C:\\Users\\sakshigarg\\Desktop\\Prerna_BTP_Liver disease_2018\\Dataset\\dataset\\"
-
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 for i in range(0,131):
-    img = Image.open(fil1 + 'seg'+str(i)+'a.png')
+    img = Image.open(source + 'seg'+str(i)+'a.png')
     arr=np.array(img)
     liver = np.copy(arr)
     lesion = np.copy(arr)
@@ -12,15 +13,15 @@ for i in range(0,131):
     liver[liver<100] = 0
     liver[liver>=100] = 255
     imgsave = Image.fromarray(liver)
-    imgsave.save(fil1+'segliver'+str(i)+'a.png')
+    imgsave.save(destination+'segliver'+str(i)+'a.png')
 
     lesion[lesion<=200] = 0
     lesion[lesion>200] = 255
     imgsave = Image.fromarray(lesion)
-    imgsave.save(fil1+'seglesion'+str(i)+'a.png')
+    imgsave.save(destination+'seglesion'+str(i)+'a.png')
 
 
-    img = Image.open(fil1 + 'seg' + str(i) + 'c.png')
+    img = Image.open(source + 'seg' + str(i) + 'c.png')
     arr = np.array(img)
     liver = np.copy(arr)
     lesion = np.copy(arr)
@@ -28,16 +29,16 @@ for i in range(0,131):
     liver[liver < 100] = 0
     liver[liver >= 100] = 255
     imgsave = Image.fromarray(liver)
-    imgsave.save(fil1 + 'segliver' + str(i) + 'c.png')
+    imgsave.save(destination+ 'segliver' + str(i) + 'c.png')
 
     lesion[lesion <= 200] = 0
     lesion[lesion > 200] = 255
     imgsave = Image.fromarray(lesion)
-    imgsave.save(fil1 + 'seglesion' + str(i) + 'c.png')
+    imgsave.save(destination + 'seglesion' + str(i) + 'c.png')
 
 
 
-    img = Image.open(fil1 + 'seg' + str(i) + 's.png')
+    img = Image.open(source+ 'seg' + str(i) + 's.png')
     arr = np.array(img)
     liver = np.copy(arr)
     lesion = np.copy(arr)
@@ -45,9 +46,9 @@ for i in range(0,131):
     liver[liver < 100] = 0
     liver[liver >= 100] = 255
     imgsave = Image.fromarray(liver)
-    imgsave.save(fil1 + 'segliver' + str(i) + 's.png')
+    imgsave.save(destination + 'segliver' + str(i) + 's.png')
 
     lesion[lesion <= 200] = 0
     lesion[lesion > 200] = 255
     imgsave = Image.fromarray(lesion)
-    imgsave.save(fil1 + 'seglesion' + str(i) + 's.png')
+    imgsave.save(destination+ 'seglesion' + str(i) + 's.png')
